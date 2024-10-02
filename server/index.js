fd33ebe411313bbe3b053375a6c6e8d5562345b6
Ahmed Alamin <ahmedalamin957@gmail.com>
2024-10-02 17:56:25 -0400

    reverting index.js

--- server/index.js
+++ server/index.js
@@ -9,62 +9,38 @@ const { User } = require('./models/user.js');
 // Database connection
 connection();
 
-// Custom CORS middleware
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
 // Express middleware
 app.use(express.json());
-app.use(allowCors);
 
-// routes
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-
-// Fetching user
-app.get('/getUser', allowCors(async (req, res) => {
+// Fetch user
+app.get('/getUser', async (req, res) => {
     try {
         const user = await User.find();
         res.json(user);
     } catch (err) {
         res.status(500).json(err);
     }
-}));
-
-app.get('/', allowCors((req, res) => {
-    res.send('API is running...'); // Confirming Vercel connection...
-}));
+});
 
 // Add to watchlist route
-app.post('/addWatchlist', allowCors(async (req, res) => {
+app.post('/addWatchlist', async (req, res) => {
     try {
-        const user = await User.findOne(); 
+        const user = await User.findOne();
         if (!user) return res.status(404).json({ message: 'User not found' });
 
         const { imdbID, name, type } = req.body;
-        
         user.watchlist.push({ imdbID, name, type });
         await user.save();
 
         res.status(200).json({ message: 'Watchlist updated successfully', watchlist: user.watchlist });
     } catch (error) {
-        console.error('Error adding to watchlist:', error);
         res.status(500).json({ message: 'Error updating watchlist', error });
     }
-}));
+});
 
 // Starting server
 const port = process.env.PORT || 8080;
