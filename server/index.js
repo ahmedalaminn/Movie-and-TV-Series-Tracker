f7552ab362b1cf305c61273cb1edce1436a46e37
Ahmed Alamin <ahmedalamin957@gmail.com>
2024-10-02 02:31:59 -0400

    trying to deploy v3

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const { User } = require('./models/user.js');
 connection();
 
 // middlewares
-app.use(express.json());
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // routes
