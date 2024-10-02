20e4f5256a770f707dee6f0e0bf4d88c8d40ffce
Ahmed Alamin <ahmedalamin957@gmail.com>
2024-10-02 03:14:37 -0400

    debugging mongodb url

--- server/db.js
+++ server/db.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false); // Optional, depending on your query preferences
 
 module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
-	try {
-		mongoose.connect(process.env.DB, connectionParams);
-		console.log("Connected to database successfully");
-	} catch (error) {
-		console.log(error);
-		console.log("Could not connect to database");
-	}
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+
+    // Log the connection string for debugging
+    console.log("Database Connection String: ", process.env.DB);
+
+    // Using a Promise to handle connection and catch errors
+    mongoose.connect(process.env.DB, connectionParams)
+        .then(() => {
+            console.log("Connected to database successfully");
+        })
+        .catch(error => {
+            console.error("Could not connect to database:", error);
+        });
 };
